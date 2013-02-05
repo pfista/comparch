4f8dfb3268e15265585b930550a7d9b0f63ccc39
pfista <pfista@gmail.com>
2013-02-04 22:33:39 -0600

    Magic number validation, load addresses, and byte data are all being read correctly

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 comparch
 ========
+
+Each folder at root is a separate programming assignment for Computer
+Architecture CS 429
--- prog2/y86.c
+++ prog2/y86.c
@@ -2,22 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MEMORY  10000 // virtual memory size in bytes
+#define MAGIC_NUM  0x7962 // y86 signifier
+
+int validateBuffer(unsigned char* buffer, long length);
+void printBuffer(unsigned char* buffer, long length);
+
+typedef unsigned short int load_address;
+
 int main (int argc, char* argv[]) 
 {
  
-    // open file
-    //
-    // read file
-    //
-    // save file to memory
-    //
-    // print file
-    //
-    // Even though you sorted it out, I'll post an answer for others...use
-    // unsigned variables. The first byte you read will need to be signed (an
-    // int or short) so you can test for EOF. Afterwards, either cast, or use
-    // unsigned types.
-
     FILE* fp = fopen(argv[1], "rb"); // open the file based on command line argument
 
     if (fp == NULL)
@@ -34,23 +29,68 @@ int main (int argc, char* argv[])
 
         fread(buffer, 2, lSize, fp); // read in the entire file to buffer
 
-        
+        printBuffer(buffer, lSize);
+        validateBuffer(buffer, lSize);
 
-        int i;
-        printf("file size: %lu bytes\n", lSize);
-        if (buffer[0] == '0x79' && buffer[1] == '0x62')
-            printf("Magic number confirmed!\n");
-
-        for (i = 0; i < lSize; i++)
-        {
-            printf("%02x\n", buffer[i]);
-        }
-        
-        
     }
 
     printf("\n");
     fclose(fp);
 
 }
+
+/*
+ * Checks to see if the data is valid for Y86 
+ */
+int validateBuffer(unsigned char* buffer, long length) {
+
+        // Check for the magic number first
+        unsigned short int mNum = (buffer[0] << 8) + buffer[1];
+        if (mNum != MAGIC_NUM)
+            printf("mNum: %.4x\nMAGIC:%.4x\n", mNum, MAGIC_NUM);
+        else
+            printf("Magic Number Found Validated!\n");
+
+        // begin checking blocks
+        int i = 2;
+        while (i + 3 < length) {
+            load_address la = (buffer[i] << 8) + buffer[i+1];
+            unsigned short int byte_count = (buffer[i+2] << 8 ) + buffer[i+3];
+
+            printf("load_address: 0x%.4X\nbyteCount: 0x%.4X\n", la, byte_count);
+
+            i += 3;
+
+            int j;
+            for (j = 1; j <= byte_count; j++)
+            {
+                printf(" %.2x ", buffer[i+j]);
+            }
+            printf("\n");
+
+            i += 1 + byte_count;
+            printf("i updated to: 0x%.2X\n", i);
+        }
+
+
+
+}
+
+void printBuffer(unsigned char* buffer, long length)
+{
+        int i;
+        printf("file size: %lu bytes\n", length);
+
+        for (i = 0; i < length; i++)
+        {
+            printf(" %.2x", buffer[i]);
+        }
+        printf("\n");
+
+
+}
+
+
+//TODO create a memory method to store the data in memory according to the load
+//addresses
 
