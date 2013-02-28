f7399ef09c233ffb5b773ba677ab959f37c41b03
Michael Pfister <pfista@gmail.com>
2013-02-27 18:01:39 -0600

    Adding topoSort base file

---
+++ prog4/topoSort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+struct node {
+    unsigned int inDegree;
+    char* symbolName;
+    struct node *;
+    // list of other nodes
+
+
+};
+
+int main (int argc, char* argv[]) {
+
+#ifdef DEBUG
+    printf("debugging is on\n");
+#endif
+
+
+}
